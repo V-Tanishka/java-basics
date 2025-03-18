53248c4d6eee390c7af2f7d3ad01522fc0adb7ce
V-Tanishka <vashishthatanishka@gmail.com>
2025-03-18 19:19:00 +0530

    xyz: to calculate percentage

--- xyz.java
+++ xyz.java
@@ -1,5 +1,20 @@
 import org.w3c.dom.ls.LSOutput;
 
 public class xyz {
-   // LSOutput
+
+    public class que2 {
+        //que2 : calculate cgpa out of three subject
+        public static  void main(String[] args){
+            int subject1 = 57;
+            int subject2 = 58;
+            int subject3 = 39;
+            int cgpa = (subject1 + subject2 + subject3) / 30;
+            System.out.println(cgpa);
+
+        }
+
+
+    }
+
+    // LSOutput
 }
