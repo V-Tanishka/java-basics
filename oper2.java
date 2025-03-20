d7b01bcb78ba57235bb5051a11352e306c8ef24b
V-Tanishka <vashishthatanishka@gmail.com>
2025-03-20 13:27:17 +0530

    integer

---
+++ oper2.java
@@ -0,0 +1,10 @@
+public class oper2 {
+    public static void main(String[] args){
+//make a calculator to calculate the percentage
+        char grade = 'b' ;
+        grade =(char) (grade + 8);
+        System.out.println(grade);
+
+
+    }
+}
---
+++ strings.java
@@ -0,0 +1,6 @@
+public class strings {
+    public static void main(String[] args){
+        String  name = new String("harray");
+        System.out.println(name);
+    }
+}
