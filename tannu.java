6e80a84c4f9237c9c4b7e5f8840184a11436aebe
V-Tanishka <vashishthatanishka@gmail.com>
2025-03-20 20:52:00 +0530

    if-else

--- tannu.java
+++ tannu.java
@@ -1,4 +1,18 @@
 public class tannu {
-    // hello world
+    public static void main(String[] args){
+        int age = 56;
+        if(age > 56){
+            System.out.println("you are not experienced !");
+        }
+        else if(age > 46){
+            System.out.println("you are semi experienced");
+        }
+        else{
+            System.out.println("you are not experienced");
+        }
+
+    }
+
+
 }
 
