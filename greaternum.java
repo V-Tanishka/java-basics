2348b2145216d80bc954ca99702214f254a00bfb
V-Tanishka <vashishthatanishka@gmail.com>
2025-03-20 13:27:17 +0530

    integer

---
+++ greaternum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class greaternum {
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("enter the value of a");
+        int a = sc.nextInt();
+        System.out.println(a>8);
+
+    }
+}
---
+++ psofstring.java
@@ -0,0 +1,8 @@
+public class psofstring {
+   // convert the string into lower case
+    public static void main(String[] args){
+        String name = "jack parker";
+         name =(name.toLowerCase());
+        System.out.println(name);
+    }
+}
