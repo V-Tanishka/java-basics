8df976856ec470216b8510c10aa2a8b7fb01d124
V-Tanishka <vashishthatanishka@gmail.com>
2025-04-04 19:32:08 +0530

    switch

---
+++ arrayy.java
@@ -0,0 +1,12 @@
+public class arrayy {
+    public static void main(String[] args){
+        int [] marks =new int[5];
+        marks[0] = 100;
+        marks[1] = 20;
+        marks[2] = 30;
+        marks[3] = 40;
+        marks[4] = 50;
+        System.out.println(marks[0]);
+
+    }
+}
